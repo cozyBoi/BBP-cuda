7cc4c4892530ec79baae5a3326aca7976055c00d
JLee <kjjh1126@gmail.com>
2020-12-29 03:56:35 +0900

    chagne b and c

--- reduction_b.cu
+++ reduction_b.cu
@@ -11,13 +11,13 @@ __global__ void reduce0(int *g_idata, int *g_odata) {
     unsigned int i = blockIdx.x*blockDim.x + threadIdx.x;
     sdata[tid] = g_idata[i];
     __syncthreads();
-    // do reduction in shared mem
-    for(unsigned int s=1; s < blockDim.x; s *= 2) { 
-        if (tid % (2*s) == 0) {
-           sdata[tid] = sdata[tid + s] > sdata[tid] ? sdata[tid + s] : sdata[tid]; 
+    for (unsigned int s=1; s < blockDim.x; s *= 2) {
+        int index = 2 * s * tid;
+        if (index < blockDim.x) { 
+            sdata[index] = sdata[index + s] > sdata[index] ? sdata[index + s]  : sdata[index];
         }
-        __syncthreads(); 
-    }
+        __syncthreads();
+     }
     // write result for this block to global mem
     if (tid == 0) g_odata[blockIdx.x] = sdata[0];
 }
