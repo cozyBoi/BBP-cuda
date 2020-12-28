264beae241b8387557eb0b6607d478111f157f20
JLee <kjjh1126@gmail.com>
2020-12-29 03:52:21 +0900

    b done

--- reduction_a.c
+++ reduction_a.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #define N 10000
 
 int main() {
+    srand(time(NULL));
+
     int arr[N];
+    for(int i = 0; i < N; i++){
+        arr[i] = rand();
+    }
+
     int maxVal = arr[0];
     for(int i = 0; i < N; i++){
         maxVal = maxVal < arr[i] ? arr[i] : maxVal;
     }
+
+    printf("%d\n", maxVal);
     return 0;
 }
