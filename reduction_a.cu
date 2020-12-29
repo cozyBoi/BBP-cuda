dfff97a0601491e914f35d34e4d37205b7eaf072
cozyBoi <kjjh1126@sogang.ac.kr>
2020-12-29 20:56:27 +0900

    fix 3-a

--- reduction_a.cu
+++ reduction_a.cu
@@ -5,15 +5,6 @@
 
 int arr[N];
 
-int log2_N(int n){
-    int a = 1;
-    int i = 0;
-    for(i = 0; a <= n; i++){}
-        a *= 2;
-    }
-    return i;
-}
-
 int main() {
     srand(time(NULL));
 
@@ -30,7 +21,7 @@ int main() {
     int maxVal = arr[0];
     //int log2_Nnum = log_2(N);
     for (unsigned int s=1; s < N; s *= 2) {
-        for (unsigned int i=1; i < N; i++) {
+        for (unsigned int i=0; i < N; i++) {
             if (i + s < N) { 
                 arr[i] = arr[i + s] > arr[i] ? arr[i + s]  : arr[i];
             }
