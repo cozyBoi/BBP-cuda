948f465db71a84f796dc728790bdf90a31caaf16
cozyBoi <kjjh1126@sogang.ac.kr>
2020-12-29 21:32:32 +0900

    fix 3-a

--- reduction_b.cu
+++ reduction_b.cu
@@ -1,77 +1,85 @@
-#include <cuda.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <time.h>
 
-#define THREADS_PER_BLOCK 512
-#define BLOCK_NUM 16
-#define BLOCK_SIZE 16
-
-__global__ void maxReduction(int *arr, int *res)
-{
-	__shared__ int tmp[10000];
-	int tid = threadIdx.x;
-	int idx = blockIdx.x * blockDim.x + threadIdx.x;
-	tmp[tid] = arr[idx];
-
-  __syncthreads();
-	for(int i = 1; i < blockDim.x; i *= 2) {
-		if(tid % (2*i) == 0) {
-			if(tmp[tid] < tmp[tid+i])
-				tmp[tid] = tmp[tid+i];
-		}
-		__syncthreads();
-	}
-  
-  if(tid == 0)
-		res[blockIdx.x] = tmp[0];
+#define N 10000
+#define BLOCK_SIZE 32
+__global__ void reduce0(int *g_idata, int *g_odata) {
+    __shared__ int sdata[10000];
+    // each thread loads one element from global to shared mem
+    unsigned int tid = threadIdx.x;
+    unsigned int i = blockIdx.x*blockDim.x + threadIdx.x;
+    sdata[tid] = g_idata[i];
+    __syncthreads();
+    // do reduction in shared mem
+    for(unsigned int s=1; s < blockDim.x; s *= 2) { 
+        if (tid % (2*s) == 0) {
+           sdata[tid] = sdata[tid + s] > sdata[tid] ? sdata[tid + s] : sdata[tid]; 
+        }
+        __syncthreads(); 
+    }
+    // write result for this block to global mem
+    if (tid == 0) g_odata[blockIdx.x] = sdata[0];
 }
 
-int main(int argc, char *argv[])
-{
-    unsigned int size = atoi(argv[1]);
-
-    int *arr, *res;
-    arr = (int *) malloc(sizeof(int) * size);
-    res = (int *) malloc(sizeof(int) * size);
+int a[N], b[N];
+int main() {
+    srand(time(NULL));
     
-    srand(time(NULL)); 
-    for(int i = 0; i < size; i++)
-      arr[i] = rand() % size;
-  
-    int *d_arr, *d_res, *d_tmp;
-    cudaMalloc((void**) &d_arr, size * sizeof(int));
-    cudaMalloc((void**) &d_res, size * sizeof(int));
-    d_tmp = d_arr;
-
-    cudaMemcpy(d_arr, arr, size * sizeof(int), cudaMemcpyHostToDevice);
     
-    dim3 dimBlock(BLOCK_SIZE);
-    dim3 dimGrid(BLOCK_NUM);
-    
-    float time;
-    cudaEvent_t start, end;
+    cudaEvent_t start, stop;
     cudaEventCreate(&start);
-    cudaEventCreate(&end);
+    cudaEventCreate(&stop);
     
-    cudaEventRecord(start);  
+    for(int i = 0; i < N; i++){
+        a[i] = rand() % 1000;
+        //printf("%d ", a[i]);
+    }
+    //printf("\n");
 
-    do {
-      maxReduction<<<dimGrid, dimBlock>>> (d_tmp, d_res);
-      cudaMemcpy(res, d_res, size * sizeof(int), cudaMemcpyDeviceToHost);
-      d_tmp = d_res;
-      size >>= 1;
-    } while (res[1] != 0);
+    unsigned int grid_rows = (N + BLOCK_SIZE - 1) / BLOCK_SIZE;
+    unsigned int grid_cols = (N + BLOCK_SIZE - 1) / BLOCK_SIZE;
 
-    cudaEventRecord(end);
-    cudaEventSynchronize(end);
-    cudaEventElapsedTime(&time, start, end);
-    printf("Array Size: %d, Elapsed time : %.5f, Result : %d\n", size, time, res[0]);
+    dim3 dimGrid(grid_cols, grid_rows, 1); //.x
+    dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE, 1); //.y
 
-    cudaFree(d_arr);
-    cudaFree(d_res);
-    free(arr);
-    free(res);
+    
+    int*d_a, *d_b;
+    cudaMalloc((void **)&d_a, N*sizeof(int));
+    cudaMalloc((void **)&d_b, N*sizeof(int));
+    cudaMemcpy(d_a, a, N*sizeof(int), cudaMemcpyHostToDevice);
+
+    cudaEventRecord(start);
+    reduce0<<<dimGrid, dimBlock>>> (d_a, d_b);
+    cudaEventRecord(stop);
+
+    cudaMemcpy(b, d_b, N*sizeof(int), cudaMemcpyDeviceToHost);
+
+    while(1){
+        reduce0<<<dimGrid, dimBlock>>> (d_b, d_b);
+    
+        cudaMemcpy(b, d_b, N*sizeof(int), cudaMemcpyDeviceToHost);
+
+        if(b[1] == 0) break;
+    }
+    
+    /*
+    printf("a: ");
+    for(int i = 0;i < N; i++){
+        printf("%d ", a[i]);
+    }
+    printf("\n");*/
+
+    /*
+    printf("b: ");
+    for(int i = 0;i < N; i++){
+        printf("%d ", b[i]);
+    }
+    printf("\n");*/
+    cudaEventSynchronize(stop);
+    float milliseconds = 0;
+    cudaEventElapsedTime(&milliseconds, start, stop);
+    printf("time : %f\n", milliseconds);
+    printf("max : %d\n", b[0]);
     return 0;
 }
